2514796776b3c99c7fba62994ba315cd1807a310
rahul500325 <rahul.gajawada@wavemaker.com>
2025-04-24 19:47:52 +0530

    arthimetic and opertaions

--- src/main/java/org/example/ArthimeticOperators.java
+++ src/main/java/org/example/ArthimeticOperators.java
@@ -35,5 +35,28 @@ public class ArthimeticOperators {
         int studentAge = 10;
         int fullName = 6;
         int student_age = 20;
+
+        int a = 10, b = 5;
+        System.out.println(a + b); // 15
+        System.out.println(a - b); // 5
+        System.out.println(a * b); // 50
+        System.out.println(a / b); // 2
+        System.out.println(a % b); // 0
+
+        int x = 10, y = 20;
+        System.out.println(x > y);  // false
+        System.out.println(x < y);  // true
+        System.out.println(x == y); // false
+        System.out.println(x != y); // true
+
+        boolean a1 = true, b1 = false;
+        System.out.println(a1 && b1); // false
+        System.out.println(a1 || b1); // true
+        System.out.println(!a1);     // false
+
+        int m = 10;
+        m += 5;
+        m *= 2;
+        System.out.println(m); // 30
     }
 }
